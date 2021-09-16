a8d38158beb79b9c3f5c0d85349132359f2a22dc
hertonpro <alvantyomihigo@gmail.com>
2021-09-16 10:59:49 +0200

    add isset() to fund

--- resources/views/components/fund.blade.php
+++ resources/views/components/fund.blade.php
@@ -1,24 +1,25 @@
 <div>
-    <div class="bg-image rounded"
-        style="background-image: url('{{ asset('files/fundraising/'.$fundraising->image) }}'); background-size: cover; background-repeat: no-repeat; ">
+    @isset($fundraising)
+<div class="bg-image rounded"
+        style="background-image: url('{{ asset('files/fundraising/' . $fundraising->image) }}'); background-size: cover; background-repeat: no-repeat; ">
         <div class="row">
             <div class="col-lg-5"></div>
             <div class="col-lg-5 p-4 text-white ">
-                <h2 class="font-weight-bold pl-4 ">{{$fundraising->objectif}}$</h2>
-                <h3 class="pl-4">{{$fundraising->titre}}</h3>
+                <h2 class="font-weight-bold pl-4 ">{{ $fundraising->objectif }}$</h2>
+                <h3 class="pl-4">{{ $fundraising->titre }}</h3>
                 <p class="pl-4">
-                    {{$fundraising->description}}
+                    {{ $fundraising->description }}
                 </p>
                 <div class="pl-4 bg-warning p-2 rounded text-dark d-flex justify-content-between">
-                    <h4><strong>Obgectif:</strong> {{$fundraising->objectif}} $</h4>
-                    <h4><strong>Colecté:</strong> {{$colect}}</h4>
-                    <h4><strong>Reste</strong> {{$fundraising->objectif-$colect}}</h4>
+                    <h4><strong>Obgectif:</strong> {{ $fundraising->objectif }} $</h4>
+                    <h4><strong>Colecté:</strong> {{ $colect }}</h4>
+                    <h4><strong>Reste</strong> {{ $fundraising->objectif - $colect }}</h4>
                 </div>
                 <div class="d-flex flex-row-reverse p-4">
                     <form action="donation" method="POST">
                         @csrf
-                        <input type="text" hidden name="token" value="{{$token= sha1(mt_rand(1, 90000) . 'SALT') }}">
-                        <input type="number" name="fundraising" hidden value="{{$fundraising->id}}">
+                        <input type="text" hidden name="token" value="{{ $token = sha1(mt_rand(1, 90000) . 'SALT') }}">
+                        <input type="number" name="fundraising" hidden value="{{ $fundraising->id }}">
                         <button type="submit" class="btn btn-primary rounded-lg btn-lg">
                             <h4>Faire un don</h4>
                         </button>
@@ -27,4 +28,5 @@
             </div>
         </div>
     </div>
+    @endisset
 </div>
